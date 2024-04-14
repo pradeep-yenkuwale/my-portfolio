08bb4ec8f721a2bcbe04a1bb27c147a2b6b1059d
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2024-04-14 20:05:58 +0530

    Config changes - fix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const path = require('path')
 module.exports = {
   basePath: "/my-portfolio",
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
-  },
-
+  reactStrictMode: true
+  
 }
