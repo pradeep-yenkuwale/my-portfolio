b70977c5b1902835ea998210b03a03b8ea70b574
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2024-04-14 20:12:25 +0530

    Webpack fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/2048-in-react",
   output: "export",
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'media.dev.to']
+  },
 };
 
 module.exports = nextConfig;
