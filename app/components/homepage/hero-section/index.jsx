a00236afc7ad0490f5a3bf0ef3883de18e44e617
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2025-02-16 23:23:35 +0530

    label fix

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -123,7 +123,7 @@ function HeroSection() {
               <div className="ml-4 lg:ml-8 mr-2">
                 <span className=" text-white">skills:</span>
                 <span className="text-gray-400">{`['`}</span>
-c                <span className="text-amber-300">Solution and Design</span>
+                <span className="text-amber-300">Solution and Design</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">System Architecture</span>
                 <span className="text-gray-400">{"', '"}</span>
