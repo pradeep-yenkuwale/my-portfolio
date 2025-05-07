298d73d6327f0de111d8354007d507ee687c6007
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2025-05-07 12:37:38 +0530

    Fix - Profile image

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Pradeep Yenkuwale",
-  profile: '/my-portfolio/image/profile.png',
+  profile: '/image/fixedprofile.png',
   designation: "Senior Software Engineer",
   description: "My name is Pradeep Yenkuwale. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'pradeep1yenkuwale@gmail.com',
