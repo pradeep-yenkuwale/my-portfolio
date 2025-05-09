4cf49524e942516673bee40e61975ebba4ce452b
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2025-05-09 15:09:14 +0530

    Updated projects - index

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -15,7 +15,7 @@ export const projectsData = [
         image: crefin,
     },
     {
-        id: 1,
+        id: 2,
         name: 'Flight Branded Fares Recommendations',
         description: "Developed a recommendation engine that integrates multiple services, including a rule engine, which interacts with the recommendation engine to process the user search data, generate suggestions based on configured data, and adds the extra services into the supplier's branded fares and updates the results with recommended branded fares and customer can consume these results",
         tools: ['NodeJs', 'TypeScript', 'MongoDB', 'NestJs', 'Services Integration', 'System Design', "Architecture Diagram"],
@@ -25,7 +25,7 @@ export const projectsData = [
         image: crefin,
     },
     {
-        id: 2,
+        id: 3,
         name: 'Trip Advisor - AI ChatBot',
         description: "Designed and developed an AI model utilizing ChatGPT (GPT-3.5-turbo model), introduced a module for user-driven data training, and smoothly integrated it into a Python-based chatbot using the Flask framework",
         tools: ['Python', 'GPT-3.5-turbo', 'MongoDB', 'Flask', 'API Integration', 'System Design'],
@@ -35,7 +35,7 @@ export const projectsData = [
         image: crefin,
     },
     {
-        id: 3,
+        id: 4,
         name: 'PoS Integration (Clover) - boons delivery system',
         description: "Collaborated with team in building the Microservice, which will handle end to end process of order creation, payment update and order completion. Clover API Integration in the boons delivery system to automate the end to end order completion process",
         tools: ['Node.js', 'Hapi.js', 'MongoDB', 'AWS', 'API Integration', 'Joi', 'System Design'],
@@ -45,7 +45,7 @@ export const projectsData = [
         image: crefin,
     },
     {
-        id: 4,
+        id: 5,
         name: 'Delivery Management System',
         description: 'I have designed and developed a full-stack web app for managing deliveries, the application is responsible to show realtime delivery updates and other transactions, the application has the user authentication module which will enable the role based access to the delivery data',
         tools: ['Angular', 'Node.js', "Express.js", "Google Maps", "TypeScript", "MongoDB", "AWS", "API Integration", "Middleware", "NPM Package", "Microservices", "Google Services"],
@@ -55,7 +55,7 @@ export const projectsData = [
         image: travel,
     },
     {
-        id: 5,
+        id: 6,
         name: 'Web Scraper - Desktop Tool',
         description: 'Application is responsible for extracting data from the web page. Input for the tool will be list of URLs (Master URLs), stored in the database, columned with URL_ID and URL_Name. URL_ID will be used as reference_id for the extracted data from respective input URL, Tool will extract, URLs from the anchor tag(=href Attribute) in the web page and its content from the anchor tag, Tool will ignore the links which have been stored as discarded urls in the database. Extracted data will be stored in the database columned with Extracted date, URL_ID, Link, and Content',
         tools: ['Python', 'Tkinter', 'MySQL', 'Beautifulsoup', 'Express', 'Selenium', 'Webdriver', 'PyCharm'],
@@ -65,7 +65,7 @@ export const projectsData = [
         image: realEstate,
     },
     {
-        id: 6,
+        id: 7,
         name: 'Yagoot-Ware',
         description: "Desktop based application, Responsible for processing the input files (*.pch, *.set, *.bdf), which has data in string format, gather required data from the file as per the requirements and process the mathematical calculations and give the results in excel format",
         tools: ['Python', 'Tkinter', 'PyCharm'],
@@ -75,7 +75,7 @@ export const projectsData = [
         role: 'Project Engineer',
     },
     {
-        id: 7,
+        id: 8,
         name: 'Intranet - An Employee Portal',
         description: "Employees activity managment application, where employees can apply for leaves, raise technical queries, raise travel requests for corporate travels and client visits",
         tools: ['Java', 'Struts 3.1', 'MySQL', "Stored Procedure", "MySQL Triggers"],
