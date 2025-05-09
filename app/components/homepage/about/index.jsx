d56012829d44f4bbb0a1ba64cefbdac758130d70
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2025-05-09 13:08:18 +0530

    Fix

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -25,7 +25,7 @@ function AboutSection() {
         </div>
         <div className="flex justify-center order-1 lg:order-2">
           <Image
-            src={personalData.profile}
+            src="/my-portfolio/image/fixedprofile.png"
             width={280}
             height={280}
             alt="Pradeep Yenkuwale"
