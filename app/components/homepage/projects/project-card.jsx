7f1474fc152a480d5a1f99879ae2857661a50b98
Pradeep Yenkuwale <pradeep1yenkuwale@gmail.com>
2025-12-25 14:36:14 +0530

    Updated projects

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -61,6 +61,11 @@ function ProjectCard({ project }) {
             <span className="text-cyan-400">{' ' + project.description}</span>
             <span className="text-gray-400">,</span>
           </div>
+          <div className="ml-4 lg:ml-8 mr-2">
+            <span className="text-white">Feature Impact:</span>
+            <span className="text-green-400">{' ' + project.result}</span>
+            <span className="text-gray-400">,</span>
+          </div>
           <div><span className="text-gray-400">{`};`}</span></div>
         </code>
       </div>
